3d1380f5f21da1a56b122ba8b3fbe4d74389ded0
noel nyakundi <noel.nyakundi@student.moringaschool.com>
2024-03-21 01:00:20 +0300

    arithmeticdone

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
-// Write your code here
+const num1= 6.2 ;
+const num2 = 10 ;
+let multiply = num1 * num2;
+console.log( multiply);
+let random = Math.floor(Math.random() * 100);
+console.log( random);
+const num3 = 68;
+const num4 = 8;
+let mod = num3 % num4;
+console.log( mod);
+let max = Math.max(1,2,3,4,15,17,20);
+console.log( max);
